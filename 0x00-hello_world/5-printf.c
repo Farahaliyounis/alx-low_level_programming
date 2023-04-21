793cd148f60fa25a1fc0a01259b4755756490271
farah <alif92081@gmail.com>
2023-04-20 19:57:36 -0700

    zzz

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - short description
+ * description: long description
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
